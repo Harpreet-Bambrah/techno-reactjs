8761169a5c379e4acfc1efdec924faf3d23e101e
Harpreet Singh <hs289523@gmail.com>
2023-03-09 20:13:29 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import About from './About';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import Layout from './Layout'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 function App() {
   return (
   
      <BrowserRouter>
+        <Navbar/>
       <Routes>
-            <Route path='/' element={<Layout/>} />
-            <Route index element={<Home/>}/>
+            <Route path='/' element={<Home/>} />
             <Route path='/services' element={<Services/>}/>
             <Route path='/contact' element={<Contact/>}/>
             <Route path='/about' element={<About/>}/>
         
       </Routes>
+        <Footer/>
    
       </BrowserRouter>
   );
