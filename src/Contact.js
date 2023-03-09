78585cdf4663b301a0a5a707244ae20b340b0820
Harpreet Singh <hs289523@gmail.com>
2023-03-09 16:51:47 +0530

    upload

--- src/Contact.js
+++ src/Contact.js
@@ -19,26 +19,26 @@ const  Contact = () =>{
                         <div className="col-lg-7">
                             <form>
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">Enter Your Name</label>
-                                    <input type="text" class="form-control" id="name" />
+                                    <label className="form-label">Enter Your Name</label>
+                                    <input type="text" className="form-control" id="name" />
 
                                 </div>
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">Email Address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" />
+                                    <label className="form-label">Email Address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" />
 
                                 </div>
                                 <div class="mb-3">
-                                    <label for="mob" class="form-label">Mobile Number</label>
-                                    <input type="text" class="form-control" id="mob" />
+                                    <label className="form-label">Mobile Number</label>
+                                    <input type="text" className="form-control" id="mob" />
 
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                                    <label className="form-label">Message</label>
                                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                                 </div>
 
-                                <button type="submit" class="intro-btn1" style={{border:'none'}}>Submit</button>
+                                <button type="submit" className="intro-btn1" style={{border:'none'}}>Submit</button>
                             </form>
 
                         </div>
